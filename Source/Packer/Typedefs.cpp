0f2f6a49c21f614348ca68521955654ee32ec45b
Craig-Stoneham <51797383+Craig-Stoneham@users.noreply.github.com>
2023-09-01 00:47:43 +0100

    Update Typedefs

--- Source/Packer/Typedefs.cpp
+++ Source/Packer/Typedefs.cpp
@@ -21,59 +21,14 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
 /**************************************************************************/
 
-#pragma once
+#include "Typedefs.h"
 
-#include <type_traits>
-#include <string>
-#include <vector>
-#include <map>
-#include <fstream>
-#include <sstream>
-#include <iostream>
+String trim_white_space(const String& p_input) {
+    String output = p_input;
 
-#if __cplusplus >= 201703L
-#include <filesystem>
-#else // __cplusplus >= 201703L
-#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
-#include <experimental/filesystem>
-#endif // __cplusplus >= 201703L
+    output.erase(output.begin(), std::find_if(output.begin(), output.end(), [](int ch) { return !std::isspace(ch);  }));
 
-#include "Version.h"
+    output.erase(std::find_if(output.rbegin(), output.rend(), [](int ch) { return !std::isspace(ch); }).base(), output.end());
 
-// Convert into a string literal.
-#define STRINGIFY(p_x) #p_x
-#define MAKE_STRING(m_x) STRINGIFY(m_x)
-
-// Common types
-
-using String = std::string;
-
-template <class T, class ALLOC = std::allocator<T>>
-using Vector = std::vector<T, ALLOC>;
-
-template <class K, class T, class COMPARE = std::less<K>, class ALLOC = std::allocator<std::pair<const K, T>>>
-using Map = std::map<K, T, COMPARE, ALLOC>;
-
-using StringVector = Vector<String>;
-
-#if __cplusplus >= 201703L
-namespace FileAccess {
-	using namespace std::filesystem;
-};
-#else // __cplusplus >= 201703L
-namespace FileAccess {
-	using namespace std::experimental::filesystem;
-};
-#endif // __cplusplus >= 201703L
-
-using StringStream = std::stringstream;
-using StringStreamI = std::istringstream;
-using StringStreamO = std::ostringstream;
-
-using FileStreamI = std::ifstream;
-using FileStreamO = std::ofstream;
-
-static constexpr int BinaryIOS = std::ios::binary;
-
-
-String trim_white_space(const String& p_input);
+    return output;
+}
