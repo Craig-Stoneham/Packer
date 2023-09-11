c9bc1b856781f39642bb79d18db2e7c662b098d5
Craig-Stoneham <51797383+Craig-Stoneham@users.noreply.github.com>
2023-09-11 17:54:28 +0100

    Update formatting

--- Source/Tests/TestPacker.cpp
+++ Source/Tests/TestPacker.cpp
@@ -21,7 +21,6 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
 /**************************************************************************/
 
-
 #include "TestPacker.h"
 
 void TestPacker::test() {
