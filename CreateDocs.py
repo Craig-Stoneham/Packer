7f3b6268fbd718b78d9d591319e0f5c3f9633f1d
Craig-Stoneham <51797383+Craig-Stoneham@users.noreply.github.com>
2023-09-17 00:55:52 +0100

    Edit GenerateDocs.py so that Doxygen file is created by the script. Rename GenerateDocs.py to CreateDocs.py and move into the root directory. Remove Scripts directory.

---
+++ CreateDocs.py
@@ -0,0 +1,37 @@
+import subprocess
+import os
+import tempfile
+
+# Directory where this script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
+documentation_dir = os.path.join(script_dir, "Documentation")
+
+# Ensure the documentation directory exists, create it if it doesn't
+if not os.path.exists(documentation_dir):
+    os.makedirs(documentation_dir)
+
+# List of projects to document
+projects = [
+    {"name": "Packer", "input": os.path.join(script_dir, "Source/Packer"), "output": "Packer", "file_patterns": "*.h"}
+]
+
+for project in projects:
+    print(f"Generating documentation for {project['name']}...")
+    
+    # Create a temporary Doxyfile
+    with tempfile.NamedTemporaryFile(mode='w+', delete=False) as temp:
+        temp.write(f"PROJECT_NAME = {project['name']}\n")
+        temp.write(f"OUTPUT_DIRECTORY = {project['output']}\n")
+        temp.write(f"INPUT = {project['input']}\n")
+        temp.write(f"FILE_PATTERNS = {project['file_patterns']}\n")
+        temp_file_name = temp.name
+    
+    # Run Doxygen using the temporary Doxyfile
+    subprocess.run(["doxygen", temp_file_name], cwd=documentation_dir)
+    
+    # Delete the temporary Doxyfile
+    os.remove(temp_file_name)
+    
+    print(f"Documentation generated for {project['name']}.")
+
+print("Documentation generation complete.")
